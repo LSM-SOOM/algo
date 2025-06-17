2da34b622826d85d4c48f5b30890f4c8ab887dd5
LSM-SOOM <sjr02021@gmail.com>
2025-06-17 12:15:47 +0900

    [level 0] Title: flag에 따라 다른 값 반환하기, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/0/181933. flag에 따라 다른 값 반환하기/flag에 따라 다른 값 반환하기.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int a, int b, bool flag) {
+    int answer = 0;
+    
+    if (flag == true)
+        return a+b;
+    if (flag == false)
+        return a-b;
+    
+    
+    return answer;
+}
