8f19eb29d5db7999e9ca3adbb017e84c9647f146
LSM-SOOM <sjr02021@gmail.com>
2025-07-15 19:16:21 +0900

    [level 0] Title: 뒤집힌 문자열, Time: 0.01 ms, Memory: 4.11 MB -BaekjoonHub

---
+++ 프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
+char* solution(const char* my_string) {
+    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
+    int len = strlen(my_string);
+    char* answer = (char*)malloc(len+1);
+    
+    for (int i=0; i<len; i++)
+    {
+//         my_string[0]  = j .
+//         my_string[1]
+//         my_string[2]
+//         my_string[3]            
+//             출력은
+//             my_string[3]
+//             my_string[2]
+//             my_string[1]
+//             my_string[0]            
+//     출력을 제일 뒤부터 하려면 i를 0부터 했던 걸. 마지막 i부터 출력되도록
+    
+        answer[i] = my_string[len - 1 - i];
+    }
+    answer[len] = '\0';
+    
+    return answer;
+}
