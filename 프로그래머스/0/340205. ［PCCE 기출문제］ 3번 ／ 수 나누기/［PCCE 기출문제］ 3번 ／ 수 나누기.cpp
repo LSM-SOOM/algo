edc5404cff30eee9efd22ac1ffe48bdd414a29c4
LSM-SOOM <sjr02021@gmail.com>
2025-08-27 19:35:57 +0900

    Update ［PCCE 기출문제］ 3번 ／ 수 나누기.cpp

--- 프로그래머스/0/340205. ［PCCE 기출문제］ 3번 ／ 수 나누기/［PCCE 기출문제］ 3번 ／ 수 나누기.cpp
+++ 프로그래머스/0/340205. ［PCCE 기출문제］ 3번 ／ 수 나누기/［PCCE 기출문제］ 3번 ／ 수 나누기.cpp
@@ -7,7 +7,8 @@ int main(void) {
     cin >> number;
     
     int answer = 0;
-    
+
+    #for(int i=0; i<1; i++)
     while(number > 0) { 
         answer += number % 100;
         number /= 100;
